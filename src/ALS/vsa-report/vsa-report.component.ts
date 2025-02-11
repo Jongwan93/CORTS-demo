f809fa87c2018888d50391b3b2af0dc7a74c7982
tchung4 <tammy.chung4@ontario.ca>
2025-02-11 15:59:28 -0500

    VSA/ASL required fields check and submit

--- src/ALS/vsa-report/vsa-report.component.ts
+++ src/ALS/vsa-report/vsa-report.component.ts
@@ -1,18 +1,18 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
-import { Title } from "@angular/platform-browser";
+import { RouterLink, RouterModule, Router } from '@angular/router';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-vsa-report',
   standalone: true,
   imports: [FormsModule, RouterModule],
   templateUrl: './vsa-report.component.html',
-  styleUrl: './vsa-report.component.css'
+  styleUrl: './vsa-report.component.css',
 })
 export class vsaReportComponent {
-  constructor(private titleService: Title) {
-    this.titleService.setTitle("CORTS - COR Entry (New)");
+  constructor(private titleService: Title, private router:Router) {
+    this.titleService.setTitle('CORTS - COR Entry (New)');
   }
 
   relatedCORInput: string = '';
@@ -33,33 +33,64 @@ export class vsaReportComponent {
     vsaType: '',
     vsaDate: '',
     vsaTime: '',
-    vsaDescription: ''
+    vsaDescription: '',
   };
 
-  vsaElements = document.getElementsByClassName('vsa-req-input') as HTMLCollectionOf<HTMLElement>;
+  reqVSAElements = document.getElementsByClassName(
+    'vsa-req-input'
+  ) as HTMLCollectionOf<HTMLElement>;
 
   isVSASelected = false;
+  isALSSelected = false;
 
-  togglePronouncedBy() {
-    this.isVSASelected = !this.isVSASelected;
-    this.vsaReqInput();
-  }
-
-  vsaReqInput(): void{
-    if(this.isVSASelected){
-      for(let i=0; i<this.vsaElements.length; i++){
-        const vsaElement = this.vsaElements[i]
-        vsaElement.classList.add('req-input')
-      }
-    }else
-    {
-      for(let i=0; i<this.vsaElements.length; i++){
-        const vsaElement = this.vsaElements[i]
-        vsaElement.classList.remove('req-input')
-      }
+  vsaTypeCheck(): boolean {
+    if (this.isALSSelected || this.isVSASelected) {
+      console.log('Report type selected');
+      return true;
+    } else {
+      console.error('You must select either ALS, VSA or both.');
+      window.alert('Error: You must select either ALS, VSA or both.');
+      return false;
     }
   }
 
+  validateReqFields(): boolean {
+    const reqFields = document.querySelectorAll('[required]');
+    for (let field of reqFields) {
+      const inputField = field as HTMLInputElement;
+      if (!inputField.value) {
+        console.error('Required field not filled:', field);
+        window.alert('Error: Not all required fields are filled in.');
+        return false;
+      }
+    }
+    return true;
+  }
+
+  toggleReportType(reportType: string): void {
+    if (reportType === 'vsa') {
+      this.isVSASelected = !this.isVSASelected;
+      this.vsaReqInput();
+    } else if (reportType === 'als') {
+      this.isALSSelected = !this.isALSSelected;
+    }
+  }
+
+  vsaReqInput(): void {
+    if (this.isVSASelected) {
+      for (let i = 0; i < this.reqVSAElements.length; i++) {
+        const reqVSAElement = this.reqVSAElements[i];
+        reqVSAElement.classList.add('req-input');
+        reqVSAElement.setAttribute('required', '');
+      }
+    } else {
+      for (let i = 0; i < this.reqVSAElements.length; i++) {
+        const vsaElement = this.reqVSAElements[i];
+        vsaElement.classList.remove('req-input');
+        vsaElement.removeAttribute('required');
+      }
+    }
+  }
 
   onSubmit(): void {
     console.log('Form submitted:', this.vsaData);
@@ -70,10 +101,22 @@ export class vsaReportComponent {
   }
 
   saveChanges(): void {
-    console.log('Changes saved');
+    if (this.vsaTypeCheck() && this.validateReqFields()) {
+      console.log('Changes Saved', this.vsaData);
+      window.alert('Changes Saved')
+    } else {
+      console.error('Error Submitting');
+    }
   }
 
   saveAndExit(): void {
-    console.log('Changes saved and exited');
+    if (this.vsaTypeCheck() && this.validateReqFields()) {
+      console.log('Changes saved and exited');
+      window.alert('Changes Saved')
+      this.router.navigate(['/mainpage']);
+    } else {
+      console.error('Error Submitting');
+    }
+
   }
 }
