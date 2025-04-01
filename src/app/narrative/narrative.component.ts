041d0d099151c4ec03c67b5d1ef925a0abc1dd02
HurSteven <steven.hur@ontario.ca>
2025-04-01 10:31:49 -0400

    narrative basic setting

---
+++ src/app/narrative/narrative.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-narrative',
+  imports: [],
+  templateUrl: './narrative.component.html',
+  styleUrl: './narrative.component.css'
+})
+export class NarrativeComponent {
+
+}
